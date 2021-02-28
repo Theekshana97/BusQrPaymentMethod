349149874e04374c84e887548b1b5c1cd20043cb
theekshana16 <60084492+theekshana16@users.noreply.github.com>
2021-02-28 07:52:17 -0800

    Initial commit

---
+++ app/src/main/java/com/example/bankdetails/BusTicketDetails.java
@@ -0,0 +1,22 @@
+package com.example.bankdetails;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class BusTicketDetails extends AppCompatActivity {
+
+    Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus_ticket_details);
+
+
+    }
+}
